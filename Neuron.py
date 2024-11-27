093290c871ee0769d0d7d46a214b4b67ed2f3a6f
Aden P <addper345@gmail.com>
2024-11-26 23:49:48 -0500

    Implement derivative calculator

--- Neuron.py
+++ Neuron.py
@@ -4,12 +4,13 @@ def sigmoid(x):
     return 1/(1+np.exp(-x))
 
 class Neuron:
-    def __init__ (self, weights, bias):
+
+    def __init__ (self, weights=[1,0], bias=0):
         self.weights = weights
         self.bias = bias
 
     def feedForward (self, input):
         return sigmoid(np.dot(self.weights, input) + self.bias)
     
-    
+
 
--- neuralNetwork.py
+++ neuralNetwork.py
@@ -7,5 +7,42 @@ class NeuralNetwork:
         self.h2 = Neuron()
         self.o1 = Neuron()
 
-    def feedForwards(self, input):
+    def feedForward(self, input):
         return self.o1.feedForward([self.h1.feedForward(input), self.h2.feedForward(input)])
+    
+    def MSE_loss(y_pred, y_true):
+        return ((y_pred-y_true) ** 2).mean()
+    
+    def calculate_deriv(self, y_pred, y_true, input):
+        fx = self.feedForward(input)
+        fx_deriv = fx*(1-fx)
+        loss_deriv = -2*(y_true - y_pred)
+
+        y_pred_deriv = fx_deriv*self.o1.weights[0]
+        foo = self.h1.feedForward(input)
+        foo_deriv = foo*(1-foo)
+
+        h1_deriv = foo_deriv*input[0]
+        w1_deriv = loss_deriv*y_pred_deriv*h1_deriv
+
+        h1_deriv = foo_deriv*input[1]
+        w2_deriv = loss_deriv*y_pred_deriv*h1_deriv
+
+        y_pred_deriv = fx_deriv*self.o1.weights[1]
+        foo = self.h2.feedForward(input)
+        foo_deriv = foo*(1-foo)
+
+        h2_deriv = foo_deriv*input[0]
+        w3_deriv = loss_deriv*y_pred_deriv*h2_deriv
+
+        h2_deriv = foo_deriv*input[1]
+        w4_deriv = loss_deriv*y_pred_deriv*h2_deriv
+
+        return [w1_deriv, w2_deriv, w3_deriv, w4_deriv]
+
+        
+        
+
+
+    
+    
