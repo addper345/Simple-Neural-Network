a2df859ddff38421bbeda5ed1459ea2400ed5bd8
Aden P <addper345@gmail.com>
2024-11-30 18:41:33 -0500

    Initialize weights to random values

--- Neuron.py
+++ Neuron.py
@@ -4,13 +4,18 @@ def sigmoid(x):
     return 1/(1+np.exp(-x))
 
 class Neuron:
+    def __init__(self):
+        self.weights = [0,0]
+        self.weights[0] = np.random.normal()
+        self.weights[1] = np.random.normal()
+        self.bias = 0
 
-    def __init__ (self, weights=[1,0], bias=0):
-        self.weights = weights
-        self.bias = bias
+    def feedForward(self, input):
+        total = sigmoid(np.dot(self.weights, np.array(input, dtype="float")) + self.bias)
+        return total
+    
 
-    def feedForward (self, input):
-        return sigmoid(np.dot(self.weights, input) + self.bias)
+        
     
 
 
